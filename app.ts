5f80ccd5d82701874c080f4442feb4e4ffc6e700
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2023-04-03 19:00:35 +0500

    production setup

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import getProductsRoutes from './routes/productsRotes';
 
 const cors = require('cors')
+const path = require('path');
 
 const app = express();
 const productsRouter = getProductsRoutes();
@@ -11,5 +12,13 @@ const middleWare = express.json();
 app.use(middleWare);
 app.use(cors());
 app.use('/products', productsRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 export default app;
